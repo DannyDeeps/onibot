ab51a74e4b304a21da08cfc58473bb96c1c7328c
Daniel Huggins <daniel.huggins@llexeter.co.uk>
2021-03-27 16:10:36 +0000

    intirole command

--- bot.php
+++ bot.php
@@ -21,7 +21,7 @@ $discord->on('ready', function(Discord $discord)
         switch (strtolower($message->content)) {
             case '!initrole':
                 $channel = $discord->getChannel('825144851267977256');
-                $promise= $channel->sendMessage(">>><br>React to this message to get your classes!<br>");
+                $promise= $channel->sendMessage(">>>  React to this message to get your classes!  ");
                 $react_array= [
                     'Heal' => ':Heal:825145748936589312',
                     'Tank' => ':Tank:825152268340953109',
