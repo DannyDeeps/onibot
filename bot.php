d4775dc796e465569758a3f288a41436be78cc8b
Daniel Huggins <daniel.huggins@llexeter.co.uk>
2021-03-27 14:13:31 +0000

    init command

--- bot.php
+++ bot.php
@@ -21,14 +21,22 @@ $discord->on('ready', function(Discord $discord)
             case '!initrole':
                 $channel = $discord->getChannel('825144851267977256');
                 $channel->sendMessage('Select a reaction to designate your role!')->done(function(Message $msg) {
-                    // $emojis= [':Heal:825145748936589312',':Tank:825357985030209576',':Range:825357985030209576',':Attack:825357985030209576'];
-                    $msg->react(':Heal:825145748936589312')->done(function(Message $msg) {
-                        $msg->react(':Tank:825357985030209576')->done(function(Message $msg) {
-                            $msg->react(':Range:825357985030209576')->done(function(Message $msg){
-                                $msg->react(':Attack:825357985030209576');
-                            });
-                        });
-                    });
+                    
+                    // $msg->react(':Heal:825145748936589312')->done(function(Message $msg) {
+                    //     $msg->react(':Tank:825357985030209576')->done(function(Message $msg) {
+                    //         $msg->react(':Range:825357985030209576')->done(function(Message $msg){
+                    //             $msg->react(':Attack:825357985030209576');
+                    //         });
+                    //     });
+                    // });
+
+
+                    $emojis= [':Heal:825145748936589312',':Tank:825357985030209576',':Range:825357985030209576',':Attack:825357985030209576'];
+                    foreach ($emojis as $emoji) {
+                        $msg->react($emoji);
+                    }
+
+
                 }, function($e) {
                     echo "Error: {$e->getMessage()}";
                 });
