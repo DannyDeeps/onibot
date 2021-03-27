f31e68cf06ac36bdab9696715b3566d254f5df1e
Daniel Huggins <daniel.huggins@llexeter.co.uk>
2021-03-27 16:00:59 +0000

    intirole command

--- bot.php
+++ bot.php
@@ -21,7 +21,7 @@ $discord->on('ready', function(Discord $discord)
         switch (strtolower($message->content)) {
             case '!initrole':
                 $channel = $discord->getChannel('825144851267977256');
-                $promise= $channel->sendMessage('Select a reaction to designate your role!');
+                $promise= $channel->sendMessage('>>> Select a reaction to designate your role!');
                 $react_array= [
                     'Heal' => ':Heal:825145748936589312',
                     'Tank' => ':Tank:825152268340953109',
