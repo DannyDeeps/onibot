724229dffbdcb77ccd8bd14961f0cf9bffb660ae
Daniel Huggins <daniel.huggins@llexeter.co.uk>
2021-03-27 14:54:06 +0000

    init command

--- bot.php
+++ bot.php
@@ -19,36 +19,46 @@ $discord->on('ready', function(Discord $discord)
     {
         switch (strtolower($message->content)) {
             case '!initrole':
-                $channel= $discord->getChannel('825144851267977256');
-                $channel->sendMessage('Select a reaction to designate your role!')->done(function(Message $msg) use ($discord, $channel) {
-                    $emojis= [':Heal:825145748936589312',':Tank:825357985030209576',':Range:825357985030209576',':Attack:825357985030209576'];
-                    $msg->reactions->push($emojis);
+                $message->channel->sendMessage($content)->done(function ($new_message) use ($message) {
+                    $promise = null;
+                    $string = '';
+                    $string1 = '$promise = $new_message->react(array_shift($react_array))->done(function () use ($react_array, $i, $new_message) {';
+                    $string2 = '});';
+                    for ($i = 0; $i < count($react_array); $i++) {
+                      $string .= $string1;
+                    }
+                    for ($i = 0; $i < count($react_array); $i++) {
+                      $string .= $string2;
+                    }
+                    eval($string); //I really hate this language sometimes
+                }
+                // $channel->sendMessage('Select a reaction to designate your role!')->done(function(Message $message) use ($discord, $channel) {
                     
 
-                    // Solution #1
-                    // $msg->react(':Heal:825145748936589312')->done(function(Message $msg) {
-                    //     $msg->react(':Tank:825357985030209576')->done(function(Message $msg) {
-                    //         $msg->react(':Range:825357985030209576')->done(function(Message $msg){
-                    //             $msg->react(':Attack:825357985030209576');
-                    //         });
-                    //     });
-                    // });
+                //     // Solution #1
+                //     // $msg->react(':Heal:825145748936589312')->done(function(Message $msg) {
+                //     //     $msg->react(':Tank:825357985030209576')->done(function(Message $msg) {
+                //     //         $msg->react(':Range:825357985030209576')->done(function(Message $msg){
+                //     //             $msg->react(':Attack:825357985030209576');
+                //     //         });
+                //     //     });
+                //     // });
 
 
-                    // Solution #2
-                    // $emojis= [':Heal:825145748936589312',':Tank:825357985030209576',':Range:825357985030209576',':Attack:825357985030209576'];
-                    // foreach ($emojis as $emoji) {
-                    //     $msg->react($emoji);
-                    // }
+                //     // Solution #2
+                //     // $emojis= [':Heal:825145748936589312',':Tank:825357985030209576',':Range:825357985030209576',':Attack:825357985030209576'];
+                //     // foreach ($emojis as $emoji) {
+                //     //     $msg->react($emoji);
+                //     // }
 
-                    // Solution #3
-                    // $channel->getMessage($msg->id)->done(function(Message $msg){
-                    //     $emojis= [':Heal:825145748936589312',':Tank:825357985030209576',':Range:825357985030209576',':Attack:825357985030209576'];
-                    //     $msg->reactions= $emojis;
-                    // });
-                }, function($e) {
-                    echo "Error: {$e->getMessage()}";
-                });
+                //     // Solution #3
+                //     // $channel->getMessage($msg->id)->done(function(Message $msg){
+                //     //     $emojis= [':Heal:825145748936589312',':Tank:825357985030209576',':Range:825357985030209576',':Attack:825357985030209576'];
+                //     //     $msg->reactions= $emojis;
+                //     // });
+                // }, function($e) {
+                //     echo "Error: {$e->getMessage()}";
+                // });
                 break;
             case '!initregion':
                 $channel = $discord->getChannel('825144851267977256');
