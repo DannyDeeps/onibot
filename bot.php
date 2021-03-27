6b371d9a7f99fb3935d446e60ad241991822d9e9
Daniel Huggins <daniel.huggins@llexeter.co.uk>
2021-03-27 22:08:27 +0000

    intirole command

--- bot.php
+++ bot.php
@@ -82,22 +82,22 @@ $discord->on('ready', function(Discord $discord)
         if (!$reaction->member->user->bot && $reaction->channel_id == Channels::ROLES) {
             switch ($reaction->emoji->name) {
                 case 'Heal':
-                    $reaction->member->addRole(Roles::HEAL)->done(null, function($e) { echo "ERROR: {$e->getMessage()} | Line [".__LINE__."]"; });
+                    $reaction->member->addRole(Roles::HEAL)->done(null, function($e) { echo "ERROR: {$e->getMessage()} | Line [".__LINE__."]\r\n"; });
                     break;
                 case 'Tank':
-                    $reaction->member->addRole(Roles::TANK)->done(null, function($e) { echo "ERROR: {$e->getMessage()} | Line [".__LINE__."]"; });
+                    $reaction->member->addRole(Roles::TANK)->done(null, function($e) { echo "ERROR: {$e->getMessage()} | Line [".__LINE__."]\r\n"; });
                     break;
                 case 'Range':
-                    $reaction->member->addRole(Roles::RANGE)->done(null, function($e) { echo "ERROR: {$e->getMessage()} | Line [".__LINE__."]"; });
+                    $reaction->member->addRole(Roles::RANGE)->done(null, function($e) { echo "ERROR: {$e->getMessage()} | Line [".__LINE__."]\r\n"; });
                     break;
                 case 'Attack':
-                    $reaction->member->addRole(Roles::ATTACK)->done(null, function($e) { echo "ERROR: {$e->getMessage()} | Line [".__LINE__."]"; });
+                    $reaction->member->addRole(Roles::ATTACK)->done(null, function($e) { echo "ERROR: {$e->getMessage()} | Line [".__LINE__."]\r\n"; });
                     break;
                 case 'EU':
-                    $reaction->member->addRole(Roles::EU)->done(null, function($e) { echo "ERROR: {$e->getMessage()} | Line [".__LINE__."]"; });
+                    $reaction->member->addRole(Roles::EU)->done(null, function($e) { echo "ERROR: {$e->getMessage()} | Line [".__LINE__."]\r\n"; });
                     break;
                 case 'NA':
-                    $reaction->member->addRole(Roles::NA)->done(null, function($e) { echo "ERROR: {$e->getMessage()} | Line [".__LINE__."]"; });
+                    $reaction->member->addRole(Roles::NA)->done(null, function($e) { echo "ERROR: {$e->getMessage()} | Line [".__LINE__."]\r\n"; });
                     break;
             }
         }
@@ -109,22 +109,22 @@ $discord->on('ready', function(Discord $discord)
         if (!$reaction->member->user->bot && $reaction->channel_id == Channels::ROLES) {
             switch ($reaction->emoji->name) {
                 case 'Heal':
-                    $reaction->member->removeRole(Roles::HEAL)->done(null, function($e) { echo "ERROR: {$e->getMessage()} | Line [".__LINE__."]"; });
+                    $reaction->member->removeRole(Roles::HEAL)->done(null, function($e) { echo "ERROR: {$e->getMessage()} | Line [".__LINE__."]\r\n"; });
                     break;
                 case 'Tank':
-                    $reaction->member->removeRole(Roles::TANK)->done(null, function($e) { echo "ERROR: {$e->getMessage()} | Line [".__LINE__."]"; });
+                    $reaction->member->removeRole(Roles::TANK)->done(null, function($e) { echo "ERROR: {$e->getMessage()} | Line [".__LINE__."]\r\n"; });
                     break;
                 case 'Range':
-                    $reaction->member->removeRole(Roles::RANGE)->done(null, function($e) { echo "ERROR: {$e->getMessage()} | Line [".__LINE__."]"; });
+                    $reaction->member->removeRole(Roles::RANGE)->done(null, function($e) { echo "ERROR: {$e->getMessage()} | Line [".__LINE__."]\r\n"; });
                     break;
                 case 'Attack':
-                    $reaction->member->removeRole(Roles::ATTACK)->done(null, function($e) { echo "ERROR: {$e->getMessage()} | Line [".__LINE__."]"; });
+                    $reaction->member->removeRole(Roles::ATTACK)->done(null, function($e) { echo "ERROR: {$e->getMessage()} | Line [".__LINE__."]\r\n"; });
                     break;
                 case 'EU':
-                    $reaction->member->removeRole(Roles::EU)->done(null, function($e) { echo "ERROR: {$e->getMessage()} | Line [".__LINE__."]"; });
+                    $reaction->member->removeRole(Roles::EU)->done(null, function($e) { echo "ERROR: {$e->getMessage()} | Line [".__LINE__."]\r\n"; });
                     break;
                 case 'NA':
-                    $reaction->member->removeRole(Roles::NA)->done(null, function($e) { echo "ERROR: {$e->getMessage()} | Line [".__LINE__."]"; });
+                    $reaction->member->removeRole(Roles::NA)->done(null, function($e) { echo "ERROR: {$e->getMessage()} | Line [".__LINE__."]\r\n"; });
                     break;
             }
         }
@@ -147,7 +147,7 @@ $discord->on('ready', function(Discord $discord)
             echo "ERROR: {$e->getMessage()} | Line [".__LINE__."]";
         });
     }, function($e) {
-        echo "ERROR: {$e->getMessage()} | Line [".__LINE__."]";
+        echo "ERROR: {$e->getMessage()} | Line [".__LINE__."]\r\n";
     });
 });
 
