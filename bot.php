1ad5b1c9d66cff606aa49b3f9278576d422197a1
Daniel Huggins <daniel.huggins@llexeter.co.uk>
2021-03-27 04:05:44 +0000

    initrole command

--- bot.php
+++ bot.php
@@ -9,22 +9,24 @@ use React\Http\Browser;
 require __DIR__ . '/vendor/autoload.php';
 
 $loop = Factory::create();
-$browser= new Browser($loop);
+// $browser= new Browser($loop);
 $discord = new Discord([
     'token' => 'ODI1MTU0MTc3NzcyMTU5MDM2.YF5ytg.c6tGhN9N4XPDJQnxeXU0An3EtFw',
     'loop' => $loop,
     'disabledEvents' => []
 ]);
 
-$discord->on('ready', function(Discord $discord) use ($browser) {
-    $discord->on('message', function (Message $message, Discord $discord) use ($browser) {
+$discord->on('ready', function(Discord $discord) {
+    $discord->on('message', function (Message $message, Discord $discord) {
         switch (strtolower($message->content)) {
             case '!initrole':
-                $message->reply('boobs');
-                $discord->guilds->fetch('ONI')->done(function(Guild $guild) {
-                    $guild->channels->fetch('roles')->done(function(Channel $channel) {
-                        die('<pre>'.print_r($channel,true).'</pre>');
-                    });
+                $discord->guilds->fetch('ONI')->done(function(Guild $guild) use ($message) {
+                    $content= json_encode($guild);
+                    $message->reply($content);
+
+                    // $guild->channels->fetch('roles')->done(function(Channel $channel) {
+                    //     die('<pre>'.print_r($channel,true).'</pre>');
+                    // });
                 });
                 break;
         }
